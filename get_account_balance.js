5d2ffd8e3aa6ae671e4c484dadc519f0b7428721
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-04-21 09:44:39 +0530

    Created a wallet and added accounts

--- get_account_balance.js
+++ get_account_balance.js
@@ -1,8 +1,8 @@
 let Web3 = require('web3')
-let url = "https://rinkeby.infura.io/v3/758990e5645944eab4d465a8eb6098d6"
+let url = "https://mainnet.infura.io/v3/758990e5645944eab4d465a8eb6098d6"
 let web3 = new Web3(url)
 
-let address = "0x6aac5e7c12d3c9259cff8e10b5bbdb1064c382a5"
+let address = "0x883486E6cc2F0f9eC7409E43E7Af24061fB2d472"
 
 web3.eth.getBalance(address, function(error, balance){
   if(!error)
